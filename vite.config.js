e07ff8a19295c474288406cc4b317f0dded00f71
Nikola Blagunovski <nikolablagunovski993@gmail.com>
2024-07-17 19:01:13 +0000

    hash router

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production';
 
-  console.log('Vite environment:', mode);
-
   return {
     base: isProduction ? '/charcoal-fe/' : '/', // Ensure correct base path
     plugins: [react()],
