205579a65e94dd6880d18a1d64efd1a5d325a8e6
Nikola Blagunovski <nikolablagunovski993@gmail.com>
2024-07-20 13:57:26 +0000

    responsivness of ourservices

--- src/utils/Layout.jsx
+++ src/utils/Layout.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
-
 import PropTypes from 'prop-types'
-
 import Footer from '../footer/Footer'
 import ContactNavigation from '../utils/ContactNavigation/ContactNavigation'
 import NavigationMenu from '../utils/NavigationMenu/NavigationMenu'
 
-// Pretpostavljamo da već imaš Footer komponentu
-
 function Layout({ children }) {
   return (
-    <div>
-      <main>{children}</main>
+    <div className="layout">
+      <main className="main-content">{children}</main>
       <ContactNavigation />
       <NavigationMenu />
       <Footer />
@@ -20,7 +16,7 @@ function Layout({ children }) {
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
 }
 
 export default Layout
